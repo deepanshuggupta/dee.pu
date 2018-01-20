524a120ecb0c1f3976d9830cd21d3e7aeddf5af9
deepanshuggupta <deepanshu.ggupta@gmail.com>
2018-01-20 19:57:08 +0530

    shortid added

--- server.js
+++ server.js
@@ -3,16 +3,20 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 var encrypt = require('./lib/encrypt');
+var shortid = require('shortid');
 
 var app = express();
 app.use(bodyParser.urlencoded({ extended: true })); 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
+var data = {};
 
+console.log(data);
 app.post('/generate', function(req, res){
-	console.log(req.body);
 	var url = req.body.url;
-	encrypt(url);
+	var value = shortid.generate();
+	data.value = url;
+	console.log(data.value);
 	return res.json("hello");
 })
 
