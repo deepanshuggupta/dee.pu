4efbe75b82fcf3d034f83028cb9a3ec4d0574a17
deepanshuggupta <deepanshu.ggupta@gmail.com>
2018-01-20 15:57:23 +0530

    kib added

--- server.js
+++ server.js
@@ -2,15 +2,20 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+var encrypt = require('./lib/encrypt');
 
 var app = express();
+app.use(bodyParser.urlencoded({ extended: true })); 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.post('/generate', function(req, res){
-	console.log(req.body.url);
+	console.log(req.body);
+	var url = req.body.url;
+	encrypt(url);
 	return res.json("hello");
 })
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
